88405a693e2a03bd61ac8249d72f19df992c8682
Kenneth Lu <krlu91@gmail.com>
2015-04-08 00:03:24 -0400

    removed extraneous comments

--- src/RPGElements/RPGUniverse/Map.py
+++ src/RPGElements/RPGUniverse/Map.py
@@ -1,15 +1,7 @@
-'''
-Created on Apr 8, 2015
-
-@author: Kenneth
-'''
-
 class Map(object):
-    '''
-    classdocs
-    '''
+
 
 
     def __init__(self, params):
-       pass
+        pass
         
